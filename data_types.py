5ccbc14efc70c5329bae483dfbed10f3a7ddda54
Kondri Indumathi <kondriindhu1@gmail.com>
2026-05-22 08:21:36 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,8 @@
+name="indhu"
+age=19
+height=5.7
+is_student=True
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
---
+++ variable.py
@@ -0,0 +1,4 @@
+name="ihndhu"
+age=19
+print(name)
+print(age)
