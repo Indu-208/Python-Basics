e607201ca7a13822047a512bf90443faec318f5b
Kondri Indumathi <kondriindhu1@gmail.com>
2026-05-24 21:08:32 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,9 @@
+numbers=[1,2,3]
+print("original list:",numbers)
+numbers.append(4)
+numbers.append(6)
+numbers.append(5)
+numbers.append(8)
+print("list after append:",numbers)
+numbers.remove(5)
+print("list after remove:",numbers)
