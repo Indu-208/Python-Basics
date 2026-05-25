c070d7399bad2c384bb6845cea6ea0db880682df
Kondri Indumathi <kondriindhu1@gmail.com>
2026-05-25 22:08:59 +0530

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,15 @@
+student = {
+    "name": "Indhu",
+    "age": 19,
+    "branch": "CSE"
+}
+print(student)
+print(student["name"])
+print(student["branch"])
+student["college"] = "ABC College"
+print(student)
+student["age"] = 20
+print(student)
+print(student.keys())
+print(student.values())
+print(student.items())
