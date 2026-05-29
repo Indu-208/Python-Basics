42e0b41a1be92ffdd2728d6008805317041fa922
Kondri Indumathi <kondriindhu1@gmail.com>
2026-05-29 19:22:47 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,3 @@
+import math_operations
+print(math_operations.add(100, 5))
+print(math_operations.sub(20, 5))
---
+++ polymorphism.py
@@ -0,0 +1,10 @@
+class Bird:
+    def sound(self):
+        print("Bird sound")
+class Cat:
+    def sound(self):
+        print("Cat sound")
+b = Bird()
+c = Cat()
+b.sound()
+c.sound()
